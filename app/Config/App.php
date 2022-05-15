864a14dfc67aec9e79270786d4f4d7e32e3c045d
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2022-05-16 00:12:10 +0700

    Revert "new yeh"
    This reverts commit 7062093ea93c866e6eec0b3c3e6b421bc67f908d.

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://tuls.korporaconsulting.com/';
+    public $baseURL = 'http://tuls.korporaconsulting.com/';
 
     /**
      * --------------------------------------------------------------------------
