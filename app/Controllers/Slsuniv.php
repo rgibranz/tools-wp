4c5a29a8d7a9d343b623a48f79021af9acd60093
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2022-05-15 21:25:15 +0700

    export course users

--- app/Controllers/Slsuniv.php
+++ app/Controllers/Slsuniv.php
@@ -7,21 +7,33 @@ use \Config\Database;
 class Slsuniv extends BaseController
 {
 
-    private $slsuniv;
+    private $posts;
+    private $usermeta;
 
     public function __construct()
     {
-        $this->slsuniv = new \App\Models\Slsuniv\Posts();
+        $this->posts = new \App\Models\Slsuniv\Posts();
+        $this->usermeta = new \App\Models\Slsuniv\Usermeta();
     }
 
     public function CourseEnrollment()
     {
-        $course = $this->slsuniv->where('post_type', 'sfwd-courses')->where('post_status', 'publish')->findAll();
+        $course = $this->posts->where('post_type', 'sfwd-courses')->where('post_status', 'publish')->findAll();
         return view('slsuniv/CourseEnrollment', ['course' => $course]);
     }
 
     public function ExportCourseEnrollment()
     {
-        var_dump($_POST);
+        $post_id = $this->request->getPost('post_id');
+
+        $course = $this->posts->find($post_id);
+
+        $db = db_connect('slsuniv');
+        $query = $db->query("SELECT * FROM wpkv_usermeta JOIN wpkv_users ON wpkv_users.id = wpkv_usermeta.user_id WHERE wpkv_usermeta.meta_key = 'course_" . $post_id . "_access_from'");
+
+        $hasil = $query->getResult();
+
+        var_dump($hasil);
+        var_dump($course);
     }
 }
